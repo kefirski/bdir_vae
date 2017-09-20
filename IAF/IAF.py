53d7002c02599d23b66af04575297d813046c8d0
kefirski <olavursky@gmail.com>
2017-09-20 11:27:34 +0300

    iaf layer added

---
+++ IAF/IAF.py
@@ -0,0 +1,48 @@
+import torch as t
+import torch.nn as nn
+
+from IAF.autoregressive_linear import AutoregressiveLinear
+from IAF.highway import Highway
+
+
+class IAF(nn.Module):
+    def __init__(self, z_size, h_size):
+        super(IAF, self).__init__()
+
+        self.z_size = z_size
+        self.h_size = h_size
+
+        self.h = Highway(self.h_size, 3, nn.ELU())
+
+        self.m = nn.Sequential(
+            AutoregressiveLinear(self.z_size + self.h_size, 2 * self.z_size, temperature=0),
+            nn.ELU(),
+            AutoregressiveLinear(2 * self.z_size, self.z_size, temperature=0)
+
+        )
+        self.s = nn.Sequential(
+            AutoregressiveLinear(self.z_size + self.h_size, 2 * self.z_size, temperature=0),
+            nn.ELU(),
+            AutoregressiveLinear(2 * self.z_size, self.z_size, temperature=0)
+
+        )
+
+    def forward(self, z, h):
+        """
+        :param z: An float tensor with shape of [batch_size, z_size]
+        :param h: An float tensor with shape of [batch_size, h_size]
+        :return: An float tensor with shape of [batch_size, z_size] and log det value of jacobian of the IAF mapping
+        """
+
+        h = self.h(h)
+
+        input = t.cat([z, h], 1)
+
+        m = self.m(input)
+        s = t.exp(self.s(input))
+
+        z = s * z + m
+
+        log_det = t.log(s + 1e-12).sum(1)
+
+        return z, log_det
---
+++ IAF/autoregressive_linear.py
@@ -0,0 +1,41 @@
+import math
+
+import torch as t
+import torch.nn as nn
+from torch.nn.init import xavier_normal
+from torch.nn.parameter import Parameter
+
+
+class AutoregressiveLinear(nn.Module):
+    def __init__(self, in_size, out_size, temperature=0, bias=True, ):
+        super(AutoregressiveLinear, self).__init__()
+
+        self.in_size = in_size
+        self.out_size = out_size
+
+        self.weight = Parameter(t.Tensor(self.in_size, self.out_size))
+
+        if bias:
+            self.bias = Parameter(t.Tensor(self.out_size))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters(temperature)
+
+    def reset_parameters(self, temperature):
+        stdv = 1. / math.sqrt(self.out_size)
+
+        self.weight = xavier_normal(self.weight)
+        self.weight = Parameter(self.weight.data.add_(temperature))
+
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input):
+        if input.dim() == 2 and self.bias is not None:
+            return t.addmm(self.bias, input, self.weight.tril(-1))
+
+        output = input @ self.weight.tril(-1)
+        if self.bias is not None:
+            output += self.bias
+        return output
